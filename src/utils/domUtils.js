9b45954eb24378c7b6b8826d318aedb8cbb7002e
Jagdish <jagadishx.upadhyay@intel.com>
2021-06-08 11:25:17 +0530

    position fixed

---
+++ src/utils/domUtils.js
@@ -0,0 +1,11 @@
+export function findPosition(obj) {
+    let curleft = 0;
+    let curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+}
