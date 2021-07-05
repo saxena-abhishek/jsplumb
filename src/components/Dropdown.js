91e2aa1d338bb999e4b83faf9d084a289664ff00
Nikhil Deochand Chakole <A122G0FD@INDIA.AIRTEL.ITM>
2021-07-05 10:21:41 +0530

    made new component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( "" );
+    }
+}
+ 
+export default Dropdown;
